201dc9a522ce981eecf89d76e1e29a6ba04b435c
Kadir <k1d12r04@gmail.com>
2022-09-05 00:35:07 +0300

    Complete media queries and fix nav layout issues

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 'use strict';
 
-const navToggle = document.getElementsByClassName("nav-toggle")[0];
-const nav = document.getElementsByClassName("nav")[0];
-
-console.log(navToggle);
-console.log(nav);
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector(".nav");
 
 navToggle.addEventListener("click", () => {
   nav.classList.toggle("nav--visible");
